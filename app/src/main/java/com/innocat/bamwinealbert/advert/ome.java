ee1bfba44688fc22883480ce9feccab761f888e6
bamwine <33618467+bamwine@users.noreply.github.com>
2023-02-06 04:21:09 -0800

    Initial commit

---
+++ app/src/main/java/com/innocat/bamwinealbert/advert/ome.java
@@ -0,0 +1,13 @@
+package com.innocat.bamwinealbert.advert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ome);
+    }
+}
